0392afd21362e667bcd6cd9dd671e4ec8e435461
Gabriela Castro <gabriela.allcastro@gmail.com>
2021-05-15 13:07:09 -0300

    wip spec controller without party

--- spec/app/controllers/travels_controller_spec.rb
+++ spec/app/controllers/travels_controller_spec.rb
@@ -2,11 +2,17 @@ describe TravelsController, type: :controller do
 
   describe "create" do
     context 'when valid params' do
-      result = HTTParty.post("http://localhost:3000/travels", body: { "destination": "cidade de destino" , 
-                    "origin": "cidade de origem", "departure_date": "2022-01-01", 
-                    "return_date": "2022-12-31" } )
+      params =
+              { 
+                "destination": "cidade de destino" , 
+                "origin": "cidade de origem", 
+                "departure_date": "2022-01-01", 
+                "return_date": "2022-12-31" 
+              }
+      before { post 'create', params: params }
+      let(:result) { JSON.parse(response.body) }
 
-      it { expect(result.code).to eq(200) }
+      it { expect(response.code).to eq("200") }
       it { expect(result['destination']).to eq("cidade de destino") }
       it { expect(result['origin']).to eq("cidade de origem") }
       it { expect(result['departure_date'].to_date.strftime("%Y-%m-%d")).to eq("2022-01-01") }
@@ -32,6 +38,12 @@ describe TravelsController, type: :controller do
       result = HTTParty.post("http://localhost:3000/travels", body: { } )
   
       it { expect(result.code).to eq(412) }
+    end
+    context 'when valid params' do
+      it 'get all travels' do
+        get 'index'
+        result = JSON.parse(response.body)
+      end
     end
   end
 end
