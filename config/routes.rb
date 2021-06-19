41fd15863a64a3016df9019aba12ff182e3dacf5
Emily Faccin <emily.faccin@gmail.com>
2021-06-19 16:07:16 -0300

    add: show route for travels controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v0 do
-      resources :travels, only: [:index, :create, :update, :destroy]
+      resources :travels, only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
